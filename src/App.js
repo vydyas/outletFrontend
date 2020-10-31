144cb468a625e134fdc3175b8f63086da041d384
Siddhu Vydabhushana <vydyas@gmail.com>
2020-10-31 22:48:31 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Autocomplete from 'react-google-autocomplete';
- 
+import axios from "axios";
 
 const App = () => {
   const [name, setName] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const onPlaceSelected = async ({ place }) => {
-    const latValue = place && place.geometry.location.lat();
-    const longValue = place && place.geometry.location.lang();
-    setIsLoading(true);
-    setError(null);
+  const onPlaceSelected = useCallback( async (place) => {
+
+    const latValue = place && place.geometry && place.geometry.location.lat();
+    const longValue = place && place.geometry && place.geometry.location.lang();
+
     try {
-      const resp = await fetch("http://localhost:8080/api/findoutlet", {
-        method: 'POST',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({lat: latValue, long: longValue}),
-      });
-      setName(resp);
-      setIsLoading(false);
-    }catch(err) {
       setIsLoading(true);
-      setError(null);
+      const respone = await axios.post("http://localhost:8080/api/findoutlet",{long: longValue, lat: latValue})
+      setName(respone.data.outlet || "No Outlet Found");
+      setIsLoading(false);
+    } catch(err) {
+      setIsLoading(false);
+      setError(err);
     }
-  };
+
+  })
   
   return (
-    <div>
-      <Autocomplete
-        style={{width: '90%'}}
-        onPlaceSelected={onPlaceSelected}
-        types={['(regions)']}
-      /><br></br>
-      {isLoading}
-      {name}
-      {error}
-    </div>
+      <>
+        <Autocomplete
+          style={{width: '90%'}}
+          onPlaceSelected = {onPlaceSelected}
+          types={['(places)']}
+        /><br/><br/>
+        {name}
+        {isLoading}
+        {error}
+      </>
   );
 };
 
